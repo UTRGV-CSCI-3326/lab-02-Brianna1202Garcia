e7efefc171d8951022464e0fbd0777f727950690
Brianna Garcia <147223105+Brianna1202Garcia@users.noreply.github.com>
2025-01-30 00:10:47 +0000

    done

---
+++ src/MixMatch.java
@@ -0,0 +1,29 @@
+public class MixMatch {
+    public static void main(String[] args) {
+        String name = "First";
+        System.out.println(name + "<- String, stores a collection of characters");
+
+        char letter = 'P';
+        System.out.println(letter + "<- Char, stores a single character");
+
+        short shrtNum = -2;
+        System.out.println(shrtNum + "<- Short, stores from -32,768 - 32,768");
+
+        int number = 2;
+        System.out.println(number + "<- Integer, stores from -2,147,483,648 - 2,147,483,648");
+
+        long longNum = 200000000;
+        System.out.println(longNum + "<- Long, -9223372036854775808 - 9223372036854775807");
+
+        float flNum = 220.022f;
+        System.out.println(flNum + "<- Float, 1.4e-45f - 3.4028235e+38f");
+
+        double doubNum = 3.14;
+        System.out.println(doubNum + "<- Double, -1.7976931348623157E+308 - 1.7976931348623157E+308");
+
+        boolean bool = true;
+        System.out.println(bool + "<- Boolean, true or false");
+
+        
+    }
+}
